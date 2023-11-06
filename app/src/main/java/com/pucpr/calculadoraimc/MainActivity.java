723c3b5a50e679d9e77b5644e86f28484919fbb1
Eliel trindade <123204237+elieltrindade@users.noreply.github.com>
2023-11-06 15:18:43 -0300

    implements new Activity
    activity_result and ResultActivity

--- app/src/main/java/com/pucpr/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/pucpr/calculadoraimc/MainActivity.java
@@ -1,6 +1,8 @@
 // Importa as bibliotecas necessárias.
 package com.pucpr.calculadoraimc;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,8 +15,9 @@ public class MainActivity extends AppCompatActivity {
     EditText heightEditText;
     EditText weightEditText;
     EditText resultEditText;
+    float result;
 
-    // Método chamado quando a atividade é criada.
+    // Ciclo ce vida da Activity, Método chamado quando a atividade é criada.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,42 +26,56 @@ public class MainActivity extends AppCompatActivity {
         weightEditText = findViewById(R.id.weightEditText);
         resultEditText = findViewById(R.id.resultEditText);
 
-        Log.d("lifecycle", "onCreate()"); // Registra uma mensagem de log com a identificação "lifecycle" durante a criação da atividade.
+        Log.d("lifecicle", "onCreate()"); // Registra uma mensagem de log com a identificação "lifecycle" durante a criação da atividade (util para debug no logcat).
     }
 
-    // Método chamado quando a atividade está prestes a se tornar visível.
+    // Ciclo ce vida da Activity, Método chamado quando a atividade está prestes a se tornar visível.
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("lifecycle", "onStart()"); // Registra uma mensagem de log no início do ciclo de vida da atividade.
+        Log.d("lifecicle", "onStart()"); // Registra uma mensagem de log no início do ciclo de vida da atividade.
     }
 
-    // Método chamado quando a atividade se torna visível e interativa para o usuário.
+    // Ciclo ce vida da Activity, Método chamado quando a atividade se torna visível e interativa para o usuário.
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("lifecycle", "onResume()"); // Registra uma mensagem de log quando a atividade se torna visível e interativa.
+        Log.d("lifecicle", "onResume()"); // Registra uma mensagem de log quando a atividade se torna visível e interativa.
     }
 
-    // Método chamado quando a atividade perde o foco, mas ainda é visível.
+    // Ciclo ce vida da Activity, Método chamado quando a atividade perde o foco, mas ainda é visível.
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("lifecycle", "onPause()"); // Registra uma mensagem de log quando a atividade perde o foco.
+        Log.d("lifecicle", "onPause()"); // Registra uma mensagem de log quando a atividade perde o foco.
     }
 
-    // Método chamado quando a atividade não está mais visível.
+    // Ciclo ce vida da Activity, Método chamado quando a atividade não está mais visível.
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("lifecycle", "onStop()"); // Registra uma mensagem de log quando a atividade não está mais visível.
+        Log.d("lifecicle", "onStop()"); // Registra uma mensagem de log quando a atividade não está mais visível.
     }
 
-    // Método chamado antes da destruição da atividade.
+    // Ciclo ce vida da Activity, Método chamado antes da destruição da atividade.
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("lifecycle", "onDestroy()"); // Registra uma mensagem de log antes da destruição da atividade.
+        Log.d("lifecicle", "onDestroy()"); // Registra uma mensagem de log antes da destruição da atividade.
+    }
+
+    // Método para obter um número em formato float a partir de um EditText com base no ID fornecido.
+    public float getMeasureFromEditText(int id) {
+        String aux;
+        if (id == 1) {
+            aux = weightEditText.getText().toString();
+        } if (id == 2){
+            aux = heightEditText.getText().toString();
+        }else {
+            aux = resultEditText.getText().toString();
+        }
+
+        return Float.parseFloat(aux);  // Converte o texto para float e o retorna.
     }
 
     // Método chamado quando o botão na interface do usuário é clicado e realiza uma operação matemática.
@@ -66,19 +83,11 @@ public class MainActivity extends AppCompatActivity {
         float weight = getMeasureFromEditText(1);
         float height = getMeasureFromEditText(2);
 
-        float result = weight / (height * height);
+        result = weight / (height * height);
 
-        resultEditText.setText(String.valueOf(result)); // Exibe o resultado no EditText de resultado.
-    }
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+        startActivity(intent);
 
-    // Método para obter um número em formato float a partir de um EditText com base no ID fornecido.
-    protected float getMeasureFromEditText(int id) {
-        String aux;
-        if (id == 1) {
-            aux = weightEditText.getText().toString();
-        } else {
-            aux = heightEditText.getText().toString();
-        }
-        return Float.parseFloat(aux);  // Converte o texto para float e o retorna.
+        //resultEditText.setText(String.valueOf(result)); // Exibe o resultado no EditText de resultado.
     }
 }
--- app/src/main/java/com/pucpr/calculadoraimc/ResultActivity.java
+++ app/src/main/java/com/pucpr/calculadoraimc/ResultActivity.java
@@ -3,12 +3,67 @@ package com.pucpr.calculadoraimc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+        TextView resultadoEditText;
+        TextView orientationEditText;
+        MainActivity mainActivity = new MainActivity();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        resultadoEditText = findViewById(R.id.resultEditText);
+        orientationEditText = findViewById(R.id.orientationEditText);
+
+        resultImc();
     }
+
+    public void resultImc() {
+        float result = mainActivity.result;
+       //float result = mainActivity.getMeasureFromEditText(3);
+        //float resultado = getMeasureFloatFromEditText();
+
+        if (16 > result) {
+            resultadoEditText.setText("Magreza Grau III");
+            orientationEditText.setText("");
+        }
+        if (result >= 16 && result <= 1.69) {
+            resultadoEditText.setText("Magreza Grau II");
+            orientationEditText.setText("");
+        }
+        if (result >= 17 && result <= 18.4) {
+            resultadoEditText.setText("Magreza Grau I");
+            orientationEditText.setText("");
+        }
+        if (result >= 18.5 && result <= 24.9) {
+            resultadoEditText.setText("Adequado");
+            orientationEditText.setText("");
+        }
+        if (result >= 25 && result <= 29.9) {
+            resultadoEditText.setText("Pré-Obeso");
+            orientationEditText.setText("");
+        }
+        if (result >= 30 && result <= 34.9) {
+            resultadoEditText.setText("Obesidade Grau I");
+            orientationEditText.setText("");
+        }
+        if (result >= 35 && result <= 39.9) {
+            resultadoEditText.setText("Obesidade Grau II");
+            orientationEditText.setText("");
+        }
+        if (result == 40) {
+            resultadoEditText.setText("Obesidade Grau III");
+            orientationEditText.setText("");
+        }
+    }
+
+        public float getMeasureFloatFromEditText() {
+            String resultado = resultadoEditText.getText().toString();
+            return Float.parseFloat(resultado);
+        }
+
 }
